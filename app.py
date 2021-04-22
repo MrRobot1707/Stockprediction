90477554264151ee6f9662b4a54b061c0501ed1e
sameershaikh <43517756+MrRobot1707@users.noreply.github.com>
2021-04-22 12:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ tickerSymbol = st.sidebar.selectbox('Stock Name', comp_list) # Select ticker sym
 StockData = yf.Ticker(tickerSymbol) # Get ticker data
 tickerDf = StockData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
 
+
+
+
 # Company information
 company_logo = '<img src=%s>' % StockData.info['logo_url']
 st.markdown(company_logo, unsafe_allow_html=True)
@@ -27,6 +30,9 @@ company_name = StockData.info['longName']
 st.header('**%s**' % company_name)
 company_MarketCap = StockData.info['marketCap']
 st.header('**MarketCap :%s**' % company_MarketCap)
+
+company_sector = StockData.info['sector']
+st.header('**Sector :%s**' % company_sector)
 
 
 # string_summary = tickerData.info['longBusinessSummary']
@@ -37,8 +43,18 @@ st.header('**Company data**')
 st.write(tickerDf)
 
 
+st.header('**Close price of stock**')
+st.write('The closing price is the last price at which the stock traded during the regular trading day.')
+st.line_chart(tickerDf.Close)
+
+st.header('**Volume of stock**')
+st.write('Volume measures the number of shares traded in a stock or contracts traded in futures or options. Volume can be an indicator of market strength, as rising markets on increasing volume are typically viewed as strong and healthy.')
+st.line_chart(tickerDf.Volume)
+
+
 # # Bollinger bands
 st.header('**Bollinger Bands**')
+st.write('Bollinger bands help determine whether prices are high or low on a relative basis. They are used in pairs, both upper and lower bands and in conjunction with a moving average.')
 qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
 qf.add_bollinger_bands()
 fig = qf.iplot(asFigure=True)
