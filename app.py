988eb2f110198e7bef35d3657308c20e030ec827
sameershaikh <43517756+MrRobot1707@users.noreply.github.com>
2021-04-24 12:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,103 +1,64 @@
 import streamlit as st
-import yfinance as yf
-import pandas as pd
-import cufflinks as cf
-import datetime
+from datetime import date
 
+import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
-# App title
-st.markdown('''# Stock Price Prediction App''')
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
+st.title('Stock Forecast App')
 
-# Sidebar
-st.sidebar.subheader('Menu')
-start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
-end_date = st.sidebar.date_input("End date", datetime.date(2021, 3, 31))
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-# Retrieving tickers data
-comp_list = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
-tickerSymbol = st.sidebar.selectbox('Stock Name', comp_list) # Select ticker symbol
-StockData = yf.Ticker(tickerSymbol) # Get ticker data
-tickerDf = StockData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
-
-year
-period = 2 *365
-
-# Company information
-company_logo = '<img src=%s>' % StockData.info['logo_url']
-st.markdown(company_logo, unsafe_allow_html=True)
-
-company_name = StockData.info['longName']
-st.header('**%s**' % company_name)
-company_MarketCap = StockData.info['marketCap']
-st.header('**MarketCap :%s**' % company_MarketCap)
-
-company_sector = StockData.info['sector']
-st.header('**Sector :%s**' % company_sector)
-
-
-# string_summary = tickerData.info['longBusinessSummary']
-# st.info(string_summary)
-
-# # Company data
-st.header('**Company data**')
-st.write(tickerDf.tail())
-
-
-st.header('**Close price of stock**')
-st.write('The closing price is the last price at which the stock traded during the regular trading day.')
-st.line_chart(tickerDf.Close)
-
-st.header('**Volume of stock**')
-st.write('Volume measures the number of shares traded in a stock or contracts traded in futures or options. Volume can be an indicator of market strength, as rising markets on increasing volume are typically viewed as strong and healthy.')
-st.line_chart(tickerDf.Volume)
-
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
 
 
 @st.cache
 def load_data(ticker):
-	data = yf.download(ticker,start_date,end_date)
-	data.reset_index(inplace=True)
-	return data
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
 
-data_load_state = st.text("loding data...")
-data = load_data(tickerSymbol)
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
-data_load_state = st.text("loding data...done")
-
+st.subheader('Raw data')
 st.write(data.tail())
 
-df_train = data[['Date','Close']]
-df_train = m.rename(columns={"Date":"ds","Close":"y"})
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
 
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
 m.fit(df_train)
-future =m.make_future_dataframe(periods=period)
+future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
-data_load_state = st.text("forecast data")
 
+# Show and plot forecast
+st.subheader('Forecast data')
 st.write(forecast.tail())
-#Forces
-
-st.write(f'Forecast plot for {year} years')
+    
+st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
-
-# # Bollinger bands
-st.header('**Bollinger Bands**')
-st.write('Bollinger bands help determine whether prices are high or low on a relative basis. They are used in pairs, both upper and lower bands and in conjunction with a moving average.')
-qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
-qf.add_bollinger_bands()
-fig = qf.iplot(asFigure=True)
-st.plotly_chart(fig)
-
-# ####
-# #st.write('---')
-# #st.write(tickerData.info)
